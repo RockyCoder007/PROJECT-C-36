cf4fd1b8322a4ee615365490adce3f0c99c78b80
RockyCoder007 <78859110+RockyCoder007@users.noreply.github.com>
2021-06-22 20:33:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+var canvas;
+
+var foodStock,foodS;
+
+
+var database;
+
+
+
+
+
+function preload(){
+    dog=loadImage("images/dog.png")
+    happyDog=loadImage("images/happyDog.png")
+    milkImg=loadImage("images/Milk.png")
+    
+}
+
+function setup(){
+  canvas = createCanvas(500,500);
+  database = firebase.database();
+
+
+  dog=createSprite(width/2,height/2)
+  dog.addImage(dog)
+  foodStock=database.ref("Food");
+  foodStock.on("value",readStock)
+}
+
+
+function draw(){
+
+   background("green");
+
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(happyDog)
+  }
+  drawSprites();
+  text("press up arrow to feed drago milk!",width/2,20)
+  text("food remaining: "+x,width/2,dog.y+50)
+  }
+  //Function to read values from DB
+function readStock(data) {
+  foodS=data. val();
+  }
+
+//Function to write values in DB
+function writeStock(x) {
+  if(x<=0){
+    x=0
+  }else{
+    x-=1
+  }
+  database. ref('/ ') .update({
+  Food : x
+  })
+}
+  
